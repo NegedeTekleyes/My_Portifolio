fa3d29698b72aaf4b31c8489f49f53d22c095b39
Negede <negedetekleyes@gmail.com>
2026-02-16 00:28:39 +0300

    initial sidebar added

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Sidebar() {
+    return (
+        <aside className="fixed left-0 top-0 h-screen w-[280px] bg-[#020617] border-r border-slate-800">
+            Sidebar
+        </aside>
+    )
+}
