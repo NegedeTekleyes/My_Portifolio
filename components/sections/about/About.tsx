59c3ee1cc61a31be76281dcb68c51b29df761043
Negede <negedetekleyes@gmail.com>
2026-02-16 00:43:53 +0300

    about section is added

---
+++ components/sections/about/About.tsx
@@ -0,0 +1,11 @@
+import { section } from "framer-motion/client";
+
+
+
+export default function About() {
+  return (
+    <section id="about" className="h-screen flex items-center justify-center">
+      About
+    </section>
+  )
+}
