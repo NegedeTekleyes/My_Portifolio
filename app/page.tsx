6d080e56ed8cb5958a01a33d9123568ced49fcf0
Negede <negedetekleyes@gmail.com>
2026-02-16 01:34:03 +0300

    Addetional component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import About from "@/components/sections/about/About";
+import Contact from "@/components/sections/contact/Contact";
+import Hero from "@/components/sections/hero/Hero";
+import Portifolio from "@/components/sections/portifolio/Portifolio";
+import Resume from "@/components/sections/resume/Resume";
+import Services from "@/components/sections/services/Services";
 
 
 export default function HomePage() {
   return (
     <>
-    
+    <Hero/>
+    <About/>
+    <Services/>
+    <Resume />
+    <Portifolio />
+    <Contact />
     </>
   )
 }
