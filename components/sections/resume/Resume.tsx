a9d3736ecdb4191696669bd112cdd76a947d73b0
Negede <negedetekleyes@gmail.com>
2026-02-16 01:30:06 +0300

    Intial Resume section added

---
+++ components/sections/resume/Resume.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Resume() {
+    return (
+        <section id="resume" className="h-screen flex items-center justify-center">
+            Resume
+        </section>
+    )
+}
