c664d1d8ffd3086791666f92e95393e1f1d4026d
Negede <negedetekleyes@gmail.com>
2026-02-16 00:35:36 +0300

    Add ne Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-import About from "@/components/About";
-import Contact from "@/components/Contact";
-import Experince from "@/components/Experince";
-import Hero from "@/components/Hero";
-import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
 
-export default function Home() {
+
+export default function HomePage() {
   return (
-    <main className="min-h-screen bg-white">
-      <Hero/>
-      <About/>
-      <Contact/>
-      <Experince/>
-      <Projects/>
-      <Skills/>
-    </main>
+    <>
+    
+    </>
   )
 }
