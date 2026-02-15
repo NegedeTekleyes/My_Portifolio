646f560262bb78feef3ba55f92d4bbdec99a111a
Negede <negedetekleyes@gmail.com>
2026-02-16 01:28:22 +0300

    Intial setup of portifolio section

---
+++ components/sections/portifolio/Portifolio.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Portifolio() {
+    return (
+        <section id="portfolio" className="h-screen flex items-center justify-center">
+            Portifolio
+        </section>
+    )
+}
