05b934cc948ef5b76ae77fdb97fea7d82a478d97
Negede <negedetekleyes@gmail.com>
2026-02-16 20:07:37 +0300

    adjust the side bar layout and profile image added

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -2,21 +2,28 @@
 
 import { navigation } from "@/data/navigation";
 import useScrollSpy from "@/hooks/useScrollSpy";
+import Image from "next/image";
 
 export default function Sidebar() {
   const active = useScrollSpy(navigation.map((n) => n.id));
 
   return (
-    <aside className="fixed left-0 top-0 h-screen w-[280px] bg-[#020617] border-r border-slate-800 flex flex-col items-center py-10">
+<aside className="hidden lg:flex fixed left-0 top-0 h-screen w-[280px] bg-[#020617] border-r border-slate-800 flex-col items-center py-10">
 
       {/* Profile */}
       <div className="text-center mb-10">
-        <div className="w-32 h-32 rounded-full bg-slate-700 mb-4" />
-        <h2 className="text-xl font-semibold">Negede Tekleyes</h2>
+        <Image 
+        src="/images/photo.jpg"
+        alt="Profile Image"
+        width={128}
+        height={128}
+        className="rounded-full border-4 border-slate-700 "
+        />
+        <h2 className="mt-6 text-xl font-semibold">Negede Tekleyes</h2>
       </div>
 
       {/* Navigation */}
-      <nav className="space-y-4 text-slate-400">
+      <nav className=" mt-10 space-y-4 text-slate-400">
         {navigation.map((item) => (
           <a
             key={item.id}
