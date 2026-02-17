2843ef5f353cfd69c33c8fd3f1fb00ad03bbe242
Negede <negedetekleyes@gmail.com>
2026-02-17 23:37:34 +0300

    sample data added

---
+++ data/data.ts
@@ -0,0 +1,50 @@
+const education = [
+  {
+    year: "2000 - 2004",
+    title: "Computer Science",
+    place: "International University",
+    desc: "Lisque persius interesset his et, in quot quidam persequeris vim."
+  },
+  {
+    year: "2005 - 2008",
+    title: "Bachelor Degree",
+    place: "University of California",
+    desc: "Lisque persius interesset his et, in quot quidam persequeris vim."
+  },
+  {
+    year: "2009 - 2012",
+    title: "Master Degree",
+    place: "Harvard University",
+    desc: "Lisque persius interesset his et, in quot quidam persequeris vim."
+  }
+];
+
+const experience = [
+  {
+    year: "2012 - 2013",
+    title: "Jr. UI UX Designer",
+    place: "Themeforest",
+    desc: "Lisque persius interesset his et, in quot quidam persequeris vim."
+  },
+  {
+    year: "2014 - 2016",
+    title: "Jr. Product Designer",
+    place: "Dribbble",
+    desc: "Lisque persius interesset his et, in quot quidam persequeris vim."
+  },
+  {
+    year: "2017 - 2019",
+    title: "Product Designer",
+    place: "Adobe",
+    desc: "Lisque persius interesset his et, in quot quidam persequeris vim."
+  }
+];
+
+const skills = [
+  { name: "Web Design", value: 65 },
+  { name: "HTML/CSS", value: 95 },
+  { name: "JavaScript", value: 80 },
+  { name: "React JS", value: 70 },
+  { name: "Angular JS", value: 60 },
+  { name: "Bootstrap", value: 99 },
+];
