7a4f5b63b43e54dc629359f06caee6fc19cc39c3
Negede <negedetekleyes@gmail.com>
2026-02-17 22:55:25 +0300

    Service section updated

--- components/sections/services/Services.tsx
+++ components/sections/services/Services.tsx
@@ -1,9 +1,23 @@
+import SectionTitle from "@/components/ui/SectionTitle";
+import ServiceCard from "@/components/ui/ServiceCard";
+import { service } from "@/data/services";
 
 
 export default function Services() {
     return (
-        <section id="services" className="h-screen flex items-center justify-center">
-            Services
+        <section id="services" className=" px-6 py-24 bg-[#202122]">
+            <SectionTitle title= "What I Do?" background="SERVICES" />
+
+            <div className="grid grid-cols-2 gap-x-20 gap-y-14">
+                {service.map((service, index) => (
+                    <ServiceCard
+                    key={index}
+                    title={service.title}
+                    desc={service.description}
+                    Icon={service.icons}
+                />
+                ))}
+            </div>
         </section>
     )
 }
