04a72c5e39f2671676a74c870a587a941747bd5b
Negede <negedetekleyes@gmail.com>
2026-02-17 23:03:25 +0300

    Services added

---
+++ data/services.ts
@@ -0,0 +1,24 @@
+import {
+    Laptop,
+    Paintbrush,
+    Search
+} from "lucide-react"
+
+export const service = [
+    {
+        title: "Web Development",
+        description: "Building responsive and modern websites using the latest technologies.",
+            icons: Laptop,
+    },
+    {
+        title: "UI/UX Design",
+        description: "Creating user-friendly and visually appealing interfaces for web applications.",
+        icons: Paintbrush,
+    },
+    {
+        title: "SEO Optimization",
+        description: "Improving website visibility and ranking on search engines.",
+        icons: Search,    
+    },
+
+]
