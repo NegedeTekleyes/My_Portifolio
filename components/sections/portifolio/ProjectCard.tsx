9cff5ab15dbaf17a58918d1cf7860f177cf08183
Negede <negedetekleyes@gmail.com>
2026-02-20 23:01:52 +0300

    add an interactive features to project cards

--- components/sections/portifolio/ProjectCard.tsx
+++ components/sections/portifolio/ProjectCard.tsx
@@ -4,19 +4,30 @@ import Image from "next/image";
 
 export default function ProjectCard({project, onClick}:{project:Project, onClick: () => void}) {
     return (
-        <div onClick={onClick} className="group cursor-pointer relative overflow-hidden rounded-xl">
+        <div onClick={onClick} className="group cursor-pointer relative overflow-hidden rounded-xl ">
             <Image 
                 src={project.images[0]} 
                 alt={project.title} 
-                width={400} 
-                height={300}
-                className="w-full h-72 object-cover transition duration-500 group-hover:scale-110"
+                width={600} 
+                height={400}
+                className="w-full aspect-video object-cover transition duration-500 group-hover:scale-110 mb-6"
             />
 
-            <div className="absolute inset-0 bg-black/70 opacity-0 group:hover:opacity-100 transition flex items-center justify-center">
-                <div className="text-center">
-                    <h3 className="text-xl font-semibold">{project.title}</h3>
-                    <p className="text-green-400 capitalize">{project.category}</p>
+            {/* Blur + Dark Overlay */}
+            <div
+                className="absolute inset-0
+                   bg-black/60 backdrop-blur-sm
+                   opacity-0 group-hover:opacity-100
+                   transition duration-300
+                   flex items-center justify-center"
+                    >   {/* text content */}
+                <div className="text-center transform translate-y-6 group-hover:translate-y-0 transition duration-300">
+                    <h3 className="text-xl font-semibold mb-2">
+                        {project.title}
+                    </h3>
+                    <p className="text-green-400 capitalize">
+                        {project.category}
+                    </p>
                 </div>
             </div>
         </div>
