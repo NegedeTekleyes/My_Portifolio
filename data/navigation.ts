936dcde56ea8b4ee58584d6aaab5645f6297f962
Negede <negedetekleyes@gmail.com>
2026-03-12 01:55:57 +0300

    update

---
+++ data/navigation.ts
@@ -0,0 +1,10 @@
+
+
+export const navigation = [
+    {label: 'Home', id: 'home'},
+    {label: 'About Me', id: 'about'},
+    {label: 'What I Do', id: 'services'},
+    {label: 'Resume', id: 'resume'},
+    {label: 'Portfolio', id: 'portfolio'},
+    {label: 'Contact', id: 'contact'},
+]
