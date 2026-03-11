1b97c8c628ad83a855765f4a8b9959728f6abafb
Negede <negedetekleyes@gmail.com>
2026-03-11 23:40:59 +0300

    Edit span tag that contains years

--- components/sections/resume/ResumeCard.tsx
+++ components/sections/resume/ResumeCard.tsx
@@ -10,13 +10,14 @@ export default function ResumeCard({ year, title, place, desc }: Props) {
         <div className="bg-[#111418] p-6 rounded-lg border border-gray-800 hover:border-green-500 transition">
            <span className="text-xs bg-red-600 px-2 py-1 rounded">
                 {year}
+                  </span>
                 <h3 className="text-lg font-semibold mt-3">{title}</h3>
 
                 <p className="text-green-400 text-sm">{place}</p>
                 <p className="text-gray-400 text-sm mt-3 leading-relaxed">
                     {desc}
                 </p>
-           </span>
+         
         </div>
     )
 }
