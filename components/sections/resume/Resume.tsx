40a7a2370f91b10abddfe99497e278bc85d6c1fa
Negede <negedetekleyes@gmail.com>
2026-03-11 23:39:56 +0300

    Update resume service background text

--- components/sections/resume/Resume.tsx
+++ components/sections/resume/Resume.tsx
@@ -1,17 +1,16 @@
 import ResumeCard from "./ResumeCard";
 import SkillBar from "./SkillBar";
 import { education, experience, skills } from "../../../data/data";
+import SectionTitle from "@/components/ui/SectionTitle";
 
 export default function Resume() {
     return (
-        <section id="resume" className="py-24 px-16 max-w-6xl mx-auto">
-          <h2 className="text-center text-4xl font-bold mb-16">
-            Resume
-            </h2>  
+        <section id="resume" className="py-24 px-6 bg-[#2E3133]">
+          <SectionTitle title="Resume" background="SUMMERY"  /> 
 
-            <div>
-                <h3>My Education</h3>
-                <div>
+            <div >
+                <h3 className="text-xl font-semibold mb-6">My Education</h3>
+                <div className="grid md:grid-cols-2 gap-6">
                     {education.map((item: any, index: any) => (
                         <ResumeCard
                         key={index} {...item}
@@ -23,7 +22,7 @@ export default function Resume() {
             {/* experince */}
             <div>
                 <h3 className="text-xl font-semibold mb-6">My Experience</h3>
-                 <div className="space-y-6">
+                 <div className="grid md:grid-cols-2 gap-6">
             {experience.map((item: any, index: any) => (
               <ResumeCard key={index} {...item} />
             ))}
