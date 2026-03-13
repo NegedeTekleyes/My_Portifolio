46f53378833b8c422ea61d82de699b8af328e95f
Negede <negedetekleyes@gmail.com>
2026-03-13 21:44:09 +0300

    add components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/layout/Footer";
 import About from "@/components/sections/about/About";
 import Contact from "@/components/sections/contact/Contact";
 import Hero from "@/components/sections/hero/Hero";
 import Portifolio from "@/components/sections/portifolio/Portifolio";
 import Resume from "@/components/sections/resume/Resume";
 import Services from "@/components/sections/services/Services";
+import ScrollTop from "@/components/ui/ScrollTop";
 
 
 export default function HomePage() {
@@ -15,6 +17,8 @@ export default function HomePage() {
     <Resume />
     <Portifolio />
     <Contact />
+    <Footer/>
+    <ScrollTop/>
     </>
   )
 }
